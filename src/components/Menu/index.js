6169674c18702cabf6eb325f6272f2fa940a864f
Athena Fernandes Sarantôpoulos <athenasaran@gmail.com>
2020-07-28 19:21:49 -0300

    aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "../../assets/img/logo.png";
 import "./Menu.css";
+import { Link } from 'react-router-dom';
 //import ButtonLink from "./components/ButtonLink";
 import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="logo" src={logo} alt="flixthena logo" />
-      </a>
-      <Button as="a" href="/" className="ButtonLink">
+      </Link>
+      
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
           Novo Vídeo
       </Button>
     </nav>
